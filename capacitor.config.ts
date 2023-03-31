fbe875ef9877aaee3203824eb6348cddc3b0e029
skalaray <127578719+skalaray@users.noreply.github.com>
2023-03-31 20:41:52 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IonicList',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
